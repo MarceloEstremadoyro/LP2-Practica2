695594187dfec2564471b390025be88cc8213d4f
Marcelo Estremadoyro <Marcelo.gozales@estudiante.ucsm.edu.pe>
2026-03-19 18:32:32 -0500

    Ep 02 Clase Carro

--- Experiencia Practica01.cpp
+++ Experiencia Practica01.cpp
@@ -34,29 +34,93 @@ public:
             cout << "Hii Hii" << endl;
         }
     }
-
 };
+
+    // CLASE CARRO
+class Carro {
+private:
+    string marca;
+    string modelo;
+    string placa;
+    int velocidad;
+    int year;
+
+public:
+    // Constructor
+    Carro(string ma, string mo, string p, int y) 
+        : marca(ma), modelo(mo), placa(p), velocidad(0), year(y) {}
+
+    // Metodo: acelerar
+    void acelerar(int incremento) {
+        velocidad += incremento;
+        cout << "El carro " << marca << " " << modelo << " acelera a " 
+             << velocidad << " km/h" << endl;
+    }
+
+    // Metodo: frenar
+    void frenar(int decremento) {
+        velocidad -= decremento;
+        if (velocidad < 0) {
+            velocidad = 0;
+        }
+        cout << "El carro " << marca << " " << modelo << " frena a " 
+             << velocidad << " km/h" << endl;
+    }
+
+    // Metodo: mostrarInfo
+    void mostrarInfo() {
+        cout << "\n--- INFO DEL CARRO ---" << endl;
+        cout << "Marca: " << marca << endl;
+        cout << "Modelo: " << modelo << endl;
+        cout << "Placa: " << placa << endl;
+        cout << "Año: " << year << endl;
+        cout << "Velocidad actual: " << velocidad << " km/h" << endl;
+    }
+};
+
+
 int main() {
-    cout << "Experiencia Practica 01:\n";
-    //PERRO
+
+    cout << "EXPERIENCIA PRACTICA 01 - POO" << endl;
+
+    //  PRUEBAS DE ANIMALES
+    cout << "PRUEBAS DE CLASE ANIMAL" << endl;
+    
     cout << "PERRO" << endl;
     Animal perro("Pug Carlino", "Canino", 3, "carne");
     perro.hacerSonido();
     perro.alimentar();
 
-    //GATO
     cout << "GATO" << endl;
-    Animal gato("Esfinge", "Felino", 26, "pescado");
+    Animal gato("Esfinge", "Felino", 2, "pescado");
     gato.hacerSonido();
     gato.alimentar();
 
-    //CABALLO
     cout << "CABALLO" << endl;
-    Animal caballo("Mustang", "Equino", 5, "Hierba");
+    Animal caballo("Mustang", "Equino", 5, "hierba");
     caballo.hacerSonido();
     caballo.alimentar();
 
-    cout << "\n fin \n" << endl;
+    //PRUEBAS DE CARROS
+    cout << "PRUEBAS DE CLASE CARRO" << endl;
+
+    cout << "CARRO 1" << endl;
+    Carro carro1("Tesla", "Model 3", "AAA-001", 2020);
+    carro1.mostrarInfo();
+    carro1.acelerar(50);
+    carro1.acelerar(30);
+    carro1.frenar(25);
+    carro1.mostrarInfo();
+
+    cout << "CARRO 2" << endl;
+    Carro carro2("Mustang", "Cobra", "BBB-002", 1969);
+    carro2.mostrarInfo();
+    carro2.acelerar(250);
+    carro2.frenar(200);
+    carro2.mostrarInfo();
+
+    cout << "FIN DE PRUEBAS";
+
     return 0;
 }
  
